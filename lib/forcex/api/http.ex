cd27008b550dbb8e474f952be429d930e3c2af10
Alex Boltunov <aboltunov@gmail.com>
2018-11-13 14:00:18 -0800

    Refactor process_response/1 for dialyzer

--- lib/forcex/api/http.ex
+++ lib/forcex/api/http.ex
@@ -15,7 +15,12 @@ defmodule Forcex.Api.Http do
   @type forcex_response :: map | {number, any} | String.t
 
   def raw_request(method, url, body, headers, options) do
-    response = method |> request!(url, body, headers, extra_options() ++ options)
+    response =
+      case request!(method, url, body, headers, extra_options() ++ options) do
+        %{body: body, status_code: 200} -> body
+        %{body: body, status_code: status_code} -> {status_code, body}
+      end
+
     Logger.debug("#{__ENV__.module}.#{elem(__ENV__.function, 0)} response=" <> inspect(response))
     response
   end
@@ -25,12 +30,12 @@ defmodule Forcex.Api.Http do
     Application.get_env(:forcex, :request_options, [])
   end
 
-  @spec process_response(HTTPoison.Response.t) :: forcex_response
-  defp process_response(%HTTPoison.Response{body: body, headers: %{"Content-Encoding" => "gzip"} = headers} = resp) do
+  @spec process_response(HTTPoison.Response.t) :: HTTPoison.Response.t
+  def process_response(%HTTPoison.Response{body: body, headers: %{"Content-Encoding" => "gzip"} = headers} = resp) do
     %{resp | body: :zlib.gunzip(body), headers: Map.drop(headers, ["Content-Encoding"])}
     |> process_response
   end
-  defp process_response(%HTTPoison.Response{body: body, headers: %{"Content-Encoding" => "deflate"} = headers} = resp) do
+  def process_response(%HTTPoison.Response{body: body, headers: %{"Content-Encoding" => "deflate"} = headers} = resp) do
     zstream = :zlib.open
     :ok = :zlib.inflateInit(zstream, -15)
     uncompressed_data = zstream |> :zlib.inflate(body) |> Enum.join
@@ -39,12 +44,11 @@ defmodule Forcex.Api.Http do
     %{resp | body: uncompressed_data, headers: Map.drop(headers, ["Content-Encoding"])}
     |> process_response
   end
-  defp process_response(%HTTPoison.Response{body: body, headers: %{"Content-Type" => "application/json" <> _} = headers} = resp) do
+  def process_response(%HTTPoison.Response{body: body, headers: %{"Content-Type" => "application/json" <> _} = headers} = resp) do
     %{resp | body: Poison.decode!(body, keys: :atoms), headers: Map.drop(headers, ["Content-Type"])}
     |> process_response
   end
-  defp process_response(%HTTPoison.Response{body: body, status_code: 200}), do: body
-  defp process_response(%HTTPoison.Response{body: body, status_code: status}), do: {status, body}
+  def process_response(response), do: response
 
   def process_request_headers(headers), do: headers ++ @user_agent ++ @accept ++ @accept_encoding
 
